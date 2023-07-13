e94ac1945f744bf8c4bbf7dbf9ffa33ec762d043
Christopher Ivy <Chrisivy22703@gmail.com>
2023-07-13 08:15:23 -0500

    Slimming Down Code

--- week1/Prop it Up/prop-it-up/src/components/PersonCard.jsx
+++ week1/Prop it Up/prop-it-up/src/components/PersonCard.jsx
@@ -1,17 +1,16 @@
 import {useState} from 'react';
 
-const PersonCard = (props) => {
-    const {firstName,lastName,hairColor} = props;
+const PersonCard = ({firstName,lastName,age,hairColor}) => {
 
-    const [ age , setAge] = useState(props.age);
+    const [ currentAge , setAge] = useState(age);
     const ageOne = () => {
-        setAge(age + 1);
+        setAge(currentAge + 1);
     }
 
     return (
         <div>
             <h1>{lastName},{firstName}</h1>
-            <p>age: {age}</p>
+            <p>age: {currentAge}</p>
             <p>Hair Color: {hairColor}</p>
             <button onClick={ ageOne }>Happy Birthday {firstName}</button>
         </div>
