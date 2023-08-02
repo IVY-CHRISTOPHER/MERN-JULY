fc853dd4b0ed8b554e1dbfc9dc994609bf113bcf
Christopher Ivy <Chrisivy22703@gmail.com>
2023-08-02 08:57:23 -0500

    Working w/ API's Created an api to count how many times it takes to get heads with a coinflip, also stops the code if it goes over 100 tries.

---
+++ week4/API's/CoinFlips.js
@@ -0,0 +1,23 @@
+function tossCoin() {
+        return Math.random() > 0.5 ? "heads" : "tails";
+    }
+
+    function fiveHeads() {
+            return new Promise( (resolve, reject) => {
+                let headsCount = 0;
+                let attempts = 0;
+                while(headsCount < 5) {
+                    attempts++;
+                    let result = tossCoin();
+                    if(result === "heads") headsCount++;
+                    else headsCount = 0;
+                    if(headsCount === 5) resolve(`It took ${attempts} tries to flip five "heads"`)
+                    if(attempts > 100) reject(`attemps reached ${attempts}, hand cramps!`)
+                }
+            });
+        }
+        fiveHeads()
+            .then( res => console.log(res) )
+            .catch( err => console.log(err) );
+        console.log( "When does this run now?" );
+        
---
+++ week4/API's/test.js
@@ -0,0 +1,32 @@
+const noMondays = new Promise( (resolve, reject) => {
+    if(new Date().getDay() !== 1) {
+        resolve("Good, it's not Monday!");
+    } else {
+        reject("Someone has a case of the Mondays!");
+    }
+});
+noMondays
+    .then( res => console.log(res) )
+    .catch( err => console.log(err) );
+
+function tossCoin() {
+        return Math.random() > 0.5 ? "heads" : "tails";
+    }
+
+function fiveHeadsSync() {
+        let headsCount = 0;
+        let attempts = 0;
+        while(headsCount < 5) {
+            attempts++;
+            let result = tossCoin();
+            console.log(`${result} was flipped`);
+            if(result === "heads") {
+                headsCount++;
+            } else {
+                headsCount = 0;
+            }
+        }
+        return `It took ${attempts} tries to flip five "heads"`;
+    }
+    console.log( fiveHeadsSync() );
+    console.log( "This is run after the fiveHeadsSync function completes" );
